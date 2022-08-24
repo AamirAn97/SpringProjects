cc16004bced5fab22a5a997e71fd818afb5b6a8b
Aamir Ansari <aamir.mahiuddin@gmail.com>
2022-08-24 11:36:40 +0530

    Packages update

--- src/main/java/com/project/spring/Springwebapplication/entity/Department.java
+++ src/main/java/com/project/spring/Springwebapplication/entity/Department.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
@@ -19,6 +20,7 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+    @NotBlank(message = "Empty Department Name Not Allowed")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
--- src/main/java/com/project/spring/Springwebapplication/service/DepartmentService.java
+++ src/main/java/com/project/spring/Springwebapplication/service/DepartmentService.java
@@ -1,4 +1,21 @@
 package com.project.spring.Springwebapplication.service;
 
+import com.project.spring.Springwebapplication.entity.Department;
+import com.project.spring.Springwebapplication.error.DepartmentNotFoundException;
+
+import java.util.List;
+
 public interface DepartmentService {
+
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    Department fetchDepartmentByName(String departmentName);
 }
